6721ed2a5d5d8f182e126ab931e742c17f88df3c
Ashish Rao <ashish.arartc@gmail.com>
2018-04-21 12:40:09 -0700

    firebase test

--- login_code.js
+++ login_code.js
@@ -1,24 +1,34 @@
 function login(email, password) {
+  var email = document.getElementById("InputEmail1").value;
+  var password = document.getElementById("InputPassword1").value;
   firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
     var errorCode = error.code;
     var errorMessage = error.message;
     alert(errorMessage)
   });
+  alert("here")
 }
 
 function sign_out() {
-  firebase.auth().signOut().then(function() {
-    console.log('Signed Out');
-  }, function(error) {
-    console.error('Sign Out Error', error);
-  })
+  firebase.auth().signOut()
 }
 
-$(document).ready(function () {
-  $("#login_button").click(function () {
-    login($("#InputEmail1").val(), $("#InputPassword1").val());
-  });
-  $("#logout_button").click(function () {
-    signOut();
-  });
-});
+// $(document).ready(function () {
+//   $("#login_button").click(function () {
+//     alert("email: " + $("#InputEmail1").val());
+//
+//     login($("#InputEmail1").val(), $("#InputPassword1").val());
+//   });
+//   $("#logout_button").click(function () {
+//     signOut();
+//   });
+// });
+
+window.onload = function() {
+  document.getElementById("login_button").onclick = function() {
+    login();
+  }
+  document.getElementById("logout_button").onclick = function() {
+    sign_out();
+  }
+}
