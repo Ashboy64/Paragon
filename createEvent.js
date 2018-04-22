44519577c26708353403fe6d3804b198b3d34a0a
pkfire <pulkitsi@gmail.com>
2018-04-22 04:05:38 -0700

    createEvent js

---
+++ createEvent.js
@@ -0,0 +1,31 @@
+function initMap(address) {
+
+    var geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode( { 'address': address}, function(results, status) {
+
+        if (status == google.maps.GeocoderStatus.OK) {
+            var latitude = results[0].geometry.location.lat();
+            var longitude = results[0].geometry.location.lng();
+        }
+
+        console.log(latitude);
+        console.log(longitude);
+
+        var myLatLng = {lat: latitude, lng: longitude};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: 'Hello World!'
+        });
+
+    });
+
+
+}
