c51d4cef8f8e618a700472b1aa869fc9e6d5f264
Ashish Rao <ashish.arartc@gmail.com>
2018-04-21 23:48:58 -0700

    sign up tests

--- signup.js
+++ signup.js
@@ -1,11 +1,37 @@
 $(document).ready(function () {
-  $("#sign_button").click(function() {
-    // $("InputPassword1").val().equiv("") ||
-    if($("#InputPassword1").val()!=$("#InputPassword2").val()){
-      $("#m_alert").css("visibility", "visible");
-    }
-  })
+  wait();
   $("#close").click(function() {
     $("#m_alert").css("visibility", "hidden");
   })
+
+  function sign_up(email, password, special) {
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorMessage)
+    });
+    var currentUser = firebase.auth().currentUser.uid;
+    var donation_ref = firebase.database().ref('users/' + currentUser + "/donation_types");
+    var location = 'users/' + currentUser;
+    if (special){
+      firebase.database().ref(location).child("special").set(true);
+    } else {
+      firebase.database().ref(location).child("special").set(false);
+    }
+
+    alert("signed up successfully");
+    wait();
+  }
+
+  function wait() {
+    $("#sign_button").click(function() {
+      // $("InputPassword1").val().equiv("") ||
+      if($("#InputPassword1").val()!=$("#InputPassword2").val()){
+        $("#m_alert").css("visibility", "visible");
+      } else {
+        sign_up($("#InputEmail1").val(), $("#InputPassword1").val(), $("#special").checked);
+      }
+    });
+  }
+
 });
